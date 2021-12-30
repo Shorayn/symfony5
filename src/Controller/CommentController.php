bbc95e7c364c73697481005a79c84edd92ccf47a
Shorayn <34161708+Shorayn@users.noreply.github.com>
2021-12-30 21:07:09 +0100

    started using services

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +14,15 @@ class CommentController extends AbstractController
     /**
      * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger) 
     {
         // TODO use id to query database
         // use real logic here to save to database
         if($direction === 'up'){
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         } else{
+            $logger->info('Voting down!');
             $currentVoteCount = rand(0, 5);   
         }
 
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
@@ -14,11 +15,14 @@ class QuestionController extends AbstractController
      *
      * @return Response
      */
-    public function homepage(): Response
+    public function homepage(Environment $twigEnvironment): Response
     {
+
+        // fun example of using the Twig service directly
+        // $html = $twigEnvironment->render('question/homepage.html.twig');
+
+        // return new Response($html);
         return $this->render('question/homepage.html.twig');
-
-
     }
 
 
@@ -37,6 +41,8 @@ class QuestionController extends AbstractController
         ];
         
         dump($this);
+
+
         return $this->render('question/show.html.twig',[
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => $answers
